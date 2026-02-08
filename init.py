a53d994ddffbc4f79134f3fa59d306ad4c24fe13
Nick <111638266+zxckolia666@users.noreply.github.com>
2026-02-08 18:40:13 +0300

    Initial commit

---
+++ init.py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 8, 1, 9]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)  # [1, 2, 5, 8, 9]
+
+# Для списка строк
+words = ["банан", "яблоко", "апельсин"]
+sorted_words = sorted(words)
+print(sorted_words)  # ['апельсин', 'банан', 'яблоко']
